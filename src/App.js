0972b6d65c82aad8364c1720d571d8cd2a705fda
Igorplisko <igorplisko95@gmail.com>
2020-11-15 00:01:19 +0200

    Main commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
-import Header from './Header';
-import Technologies from "./Technologies";
+import Header from './componets/Header';
+import Navbar from './componets/Navbar';
+import Profile from './componets/Profile';
+
 
 const App = () => {
   return (
-    <div>
-      <Header/>
-        <Technologies/>
-  </div>
-     
-  );
+    <div className="app-wrapper">
+     <Header/>
+     <Navbar/>
+     <Profile/>
+  </div>);
 }
 
 
 export default App;
 
+
+
+// https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSs4Mzi4VthlNuUgP5QbgfJyoGIPTz3Z_BylA&usqp=CAU
---
+++ src/componets/Profile.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Profile = () => {
+   return (
+
+      <div className="content">
+         <div> <img src="https://apod.nasa.gov/apod/image/2008/PerseidBridge_Zhang_4032.jpg" />
+         </div>
+
+         <div>Ava +description</div>
+
+         <div>My posts
+      <div> New post
+
+      </div>
+            <div>
+
+               <div>post1
+
+        </div>
+               <div>post2
+
+        </div>
+            </div>
+         </div>
+      </div>)
+}
+
+export default Profile
