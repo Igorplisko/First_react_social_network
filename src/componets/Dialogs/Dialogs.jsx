26a2386b3425f5d1a9d8e2c8097c1ad181c2f6a3
Igorplisko <igorplisko95@gmail.com>
2020-11-20 02:40:34 +0200

    Main commit

--- src/componets/Dialogs/Dialogs.jsx
+++ src/componets/Dialogs/Dialogs.jsx
@@ -3,9 +3,64 @@ import s from './Dialogs.module.css'
 
 const Dialogs = (props) => {
    return (
-      <div >
-         Dialogs
+
+      <div className={s.dialogs}>
+         <div className={s.dialogItems} >
+
+            <div className={s.dialog}>
+               Vladislav
+            </div>
+            <div className={s.dialog}>
+               Igor
+            </div>
+            <div className={s.dialog + ' ' + s.active}>
+               Irene
+            </div>
+            <div className={s.dialog}>
+               Margarita
+            </div>
+            <div className={s.dialog}>
+               Julia
+            </div>
+            <div className={s.dialog}>
+               Natalia
+             </div>
+            <div className={s.dialog}>
+               Gennady
+             </div>
+            <div className={s.dialog}>
+               Nikolay
+             </div>
+            <div className={s.dialog}>
+            </div>
+
+
+
+
+
+
+
+
+
+
+         </div>
+         <div className={s.messages}>
+            <div className={s.dialog}>Hey</div>
+            <div className={s.dialog}>Hi my love, How are you</div>
+            <div className={s.dialog}>I'm good ! What about you?</div>
+            <div className={s.dialog}>I'm great too.</div>
+            <div className={s.dialog}>Where are you been?</div>
+
+
+
+
+
+
+
+
+         </div>
       </div>
+
    )
 
 }
