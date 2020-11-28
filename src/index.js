ba0fd161281e88c2b70b17f50d1d157e4488fbd2
Igorplisko <igorplisko95@gmail.com>
2020-11-28 16:11:42 +0200

    commit

--- src/index.js
+++ src/index.js
@@ -3,40 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import state from './redux/state'
 
-
-let posts = [
-  { id: 1, message: 'Hey', likesCount: 25 },
-  { id: 2, message: 'Hey, how are you doing?', likesCount: 7 },
-  { id: 3, message: 'I love you ! ', likesCount: 25 },
-
-]
-
-let dialogs = [
-  { id: 1, name: 'Vladislav' },
-  { id: 2, name: 'Igor' },
-  { id: 3, name: 'Irene' },
-  { id: 4, name: 'Margarita' },
-  { id: 5, name: 'Julia' },
-  { id: 6, name: 'Natalia' },
-  { id: 7, name: 'Gennady' },
-  { id: 8, name: 'Nikolay' },
-  { id: 9, name: 'Alexander' }
-]
-
-let messages = [
-  { id: 1, message: 'Hey' },
-  { id: 2, message: 'Hi my love, How are you' },
-  { id: 3, message: 'I\'m good ! What about you?' },
-  { id: 4, message: 'I\'m great too.' },
-  { id: 5, message: 'Where are you been?' }
-
-]
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App posts={posts} dialogs={dialogs} messages={messages} />
+    <App state={state} />
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/redux/state.js
@@ -0,0 +1,39 @@
+
+
+let state = {
+   profilePage: {
+      posts: [
+         { id: 1, message: 'Hey', likesCount: 25 },
+         { id: 2, message: 'Hey, how are you doing?', likesCount: 7 },
+         { id: 3, message: 'I love you ! ', likesCount: 25 },
+
+      ]
+
+
+   },
+   dialogsPage:
+   {
+      dialogs: [
+         { id: 1, name: 'Vladislav' },
+         { id: 2, name: 'Igor' },
+         { id: 3, name: 'Irene' },
+         { id: 4, name: 'Margarita' },
+         { id: 5, name: 'Julia' },
+         { id: 6, name: 'Natalia' },
+         { id: 7, name: 'Gennady' },
+         { id: 8, name: 'Nikolay' },
+         { id: 9, name: 'Alexander' }
+      ],
+
+      messages: [
+         { id: 1, message: 'Hey' },
+         { id: 2, message: 'Hi my love, How are you' },
+         { id: 3, message: 'I\'m good ! What about you?' },
+         { id: 4, message: 'I\'m great too.' },
+         { id: 5, message: 'Where are you been?' }
+
+      ]
+   }
+}
+
+export default state;
