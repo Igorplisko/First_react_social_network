ac75217d7f124d731775e3509837748c62d6bcf0
Igorplisko <igorplisko95@gmail.com>
2020-12-01 23:52:30 +0200

    Commit

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-
+import './index.css';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 
 let rerenderEntireTree = (state) => {
