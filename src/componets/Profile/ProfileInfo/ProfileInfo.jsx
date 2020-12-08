7079cda6d4031d5a1562d1ffee0df5fb83b234e9
Igorplisko <igorplisko95@gmail.com>
2020-12-08 22:32:33 +0200

    Commit

--- src/componets/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/componets/Profile/ProfileInfo/ProfileInfo.jsx
@@ -2,22 +2,25 @@ import React from 'react';
 import s from './ProfileInfo.module.css'
 
 
-const  ProfileInfo  = () => {
+const ProfileInfo = () => {
    return (
-     <div>
-         <div> 
+      <div>
+         <div>
             <img className={s.imgStyle} src="https://sites.google.com/site/prirodanasevseegooglgfgf/_/rsrc/1463456237313/home/priroda_gory_nebo_ozero_oblaka_81150_1920x1080.jpg" />
-      </div>
+         </div>
 
-      <div className={s.descriptionBlock}>
-         Ava +description
+         <div className={s.descriptionBlock}>
+            Ava +description
          {/* Chenge this code  */}
-      </div>
-   </div> )
+         </div>
+      </div>)
 
 }
 
 
 
 
-export default ProfileInfo 
+export default ProfileInfo
+
+
+//!check and fix the code!
