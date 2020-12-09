e0c0b6a5287a5f508e49e32da0571727f9045416
Igorplisko <igorplisko95@gmail.com>
2020-12-09 22:55:36 +0200

    Mainc_Commit 12_09_2020

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-
+import React from 'react';
 import './App.css';
-// import Dialogs from './componets/Dialogs/Dialogs';
 import Header from './componets/Header/Header';
 import Navbar from './componets/Navbar/Navbar';
 import Profile from './componets/Profile/Profile';
@@ -8,33 +7,23 @@ import { Route } from "react-router-dom";
 import DialogsContainer from './componets/Dialogs/DialogsContainer';
 
 
-const App = (props) => {
-
+const App = () => {
   return (
-
-    <div className="app-wrapper">
+    <div className='app-wrapper'>
       <Header />
       <Navbar />
       <div className='app-wrapper-content'>
-        {/* <Route path="/dialogs" component={Dialogs} />
-          <Route path="/profile" component={Profile} /> */}
+        <Route path='/dialogs'
+          render={() => <DialogsContainer />} />
 
-        <Route path="/dialogs"
-          render={() => <DialogsContainer store={props.store} />} />
-        <Route path="/profile"
-          render={() => <Profile store={props.store} />} />
-
+        <Route path='/profile'
+          render={() => <Profile />} />
       </div>
     </div>
-
-  );
+  )
 }
-
 
 export default App;
 
 
 
-
-
-//!check and fix the code!
