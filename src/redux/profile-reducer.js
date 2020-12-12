011dc3ff2262426597f553c8c514ffbe0470f658
Igorplisko <igorplisko95@gmail.com>
2020-12-13 01:00:53 +0200

    Commit

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -12,24 +12,32 @@ let initialState = {
 
 }
 const profileReducer = (state = initialState, action) => {
-   debugger;
-   switch (action.type) {
-      case ADD_POST: let newPost = {
-         id: 5,
-         message: state.newPostText,
-         likesCount: 0
 
-      };
-         state.posts.push(newPost);
-         state.newPostText = '';
-         return state;
-      case UPDATE_NEW_POST_TEXT:
-         state.newPostText = action.newText;
-         return state;
+   switch (action.type) {
+      case ADD_POST: {
+         let newPost = {
+            id: 5,
+            message: state.newPostText,
+            likesCount: 0
+         };
+         let stateCopy = { ...state };
+         stateCopy.posts = [...state.posts];
+         stateCopy.posts.push(newPost);
+         stateCopy.newPostText = '';
+         return stateCopy;
+      }
+      case UPDATE_NEW_POST_TEXT: {
+
+         let stateCopy = { ...state };
+         stateCopy.posts = [...state.posts];
+         stateCopy.newPostText = action.newText;
+         return stateCopy;
+      }
       default:
          return state;
    }
 }
+
 
 
 export const addPostActionCreator = () => ({ type: ADD_POST })
