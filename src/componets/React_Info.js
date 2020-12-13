216357899e847e6c9e2f40429d6268750aca5db1
Igorplisko <igorplisko95@gmail.com>
2020-12-13 06:27:40 +0200

    Commit

--- src/componets/React_Info.js
+++ src/componets/React_Info.js
@@ -13,3 +13,10 @@
 
 //?The  "connect()" - function connects a React component to a Redux store. It does not modify the component class passed to it; instead, it returns a new, connected component class that wraps the component you passed in.
 //?The "mapStateToProps" and "mapDispatchToProps" deals with your Redux store’s state and dispatch, respectively. 
+
+
+//!1.1 "Reducer" - это чистая функция  которая принимает старый state принимает action  если необходимо то она модифицирует старый state и возврощает измененую копию или не тронутый state если его не было необходимости менять из этой функции.
+
+//!1.2Редюсер (reducer) — это чистая функция, которая принимает предыдущее состояние и экшен (state и action) и возвращает следующее состояние (новую версию предыдущего). Функция называется редюсером (reducer) потому, что ее можно передать в Array. prototype.
+
+//! pure function - не имеет права менять то что в нее пришло ...
---
+++ src/redux/users_reducer.js
@@ -0,0 +1,51 @@
+const ADD_POST = 'ADD-POST';
+const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
+
+let initialState = {
+   posts: [
+      { id: 1, message: 'Hey', likesCount: 25 },
+      { id: 2, message: 'Hey, how are you doing?', likesCount: 7 },
+      { id: 3, message: 'I love you ! ', likesCount: 25 }
+
+   ],
+   newPostText: ''
+
+}
+const profileReducer = (state = initialState, action) => {
+
+   switch (action.type) {
+      case ADD_POST: {
+         let newPost = {
+            id: 5,
+            message: state.newPostText,
+            likesCount: 0
+         };
+         return {
+            ...state,
+            posts: [...state.posts, newPost],
+            newPostText: ''
+         };
+      }
+      case UPDATE_NEW_POST_TEXT: {
+         return {
+            ...state,
+            newPostText: action.newText
+         }
+      }
+      default:
+         return state;
+   }
+}
+
+
+export const addPostActionCreator = () => ({ type: ADD_POST })
+export const updateNewPostTextActionCreator = (text) =>
+   ({ type: UPDATE_NEW_POST_TEXT, newText: text })
+
+export default profileReducer;
+
+
+
+//! "Reducer" - это чистая функция  которая принимает старый state принимает action  если необходимо то она модифицирует старый state и возврощает измененую копию или не тронутый state если его не было необходимости менять из этой функции.
+
+//! pure function - не имеет права менять то что в нее пришло ...
