e2b11811d46eb2cc412397486cbea94c48643376
Igorplisko <igorplisko95@gmail.com>
2020-12-14 02:56:04 +0200

    commit

--- src/redux/users_reducer.js
+++ src/redux/users_reducer.js
@@ -1,33 +1,75 @@
-const ADD_POST = 'ADD-POST';
-const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
+const FOLLOW = 'FOLLOW';
+const UNFOLLOW = 'UNFOLLOW';
+const SET_USERS = 'SET_USERS';
 
 let initialState = {
-   posts: [
-      { id: 1, message: 'Hey', likesCount: 25 },
-      { id: 2, message: 'Hey, how are you doing?', likesCount: 7 },
-      { id: 3, message: 'I love you ! ', likesCount: 25 }
 
-   ],
-   newPostText: ''
+   users: [
+      {
+         id: 1,
+         // photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Dmitry_Nagiev_2017_4.jpg/220px-Dmitry_Nagiev_2017_4.jpg',
+         followed: false,
+         fullName: 'Igor',
+         status: 'When if not now?',
+         location: { city: 'Minsk', country: 'Belarus' }
+      },
+      {
+         id: 2,
+         // photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Dmitry_Nagiev_2017_4.jpg/220px-Dmitry_Nagiev_2017_4.jpg',
+         followed: true,
+         fullName: 'Vlad',
+         status: 'I am a boss too',
+         location: { city: 'Moscow', country: 'Russia' }
+      },
+      {
+         id: 3,
+         // photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Dmitry_Nagiev_2017_4.jpg/220px-Dmitry_Nagiev_2017_4.jpg',
+         followed: false,
+         fullName: 'Alexsandr',
+         status: 'I am a boss too',
+         location: { city: 'Kiev', country: 'Ukraine' }
+      }
 
+   ]
 }
 const usersReducer = (state = initialState, action) => {
-
    switch (action.type) {
-      case ADD_POST: {
-
-      };
-         return
+      case FOLLOW:
+         return {
+            ...state,
+            users: state.users.map(u => {
+               if (u.id === action.userId) {
+                  return { ...u, followed: true }
+               }
+               return u;
+            })
+         }
+      case UNFOLLOW:
+         return {
+            ...state,
+            users: state.users.map(u => {
+               if (u.id === action.userId) {
+                  return { ...u, followed: false }
+               }
+               return u;
+            })
+         }
+      case SET_USERS: {
+         return { ...state, users: [...state.users, ...action.users] }
+      }
+      default:
+         return state;
    }
-
 }
 
+export const followAC = (userId) => ({ type: FOLLOW, userId })
+export const unfollowAC = (userId) => ({ type: UNFOLLOW, userId })
+export const setUsersAC = (users) => ({
+   type: SET_USERS, users
+})
 
-export const addPostActionCreator = () => ({ type: ADD_POST })
-export const updateNewPostTextActionCreator = (text) =>
-   ({ type: UPDATE_NEW_POST_TEXT, newText: text })
 
-export default profileReducer;
+export default usersReducer;
 
 
 
