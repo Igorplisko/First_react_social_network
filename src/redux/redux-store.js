d391485561967d6198a4cb602b9c9abf4a1cf1d2
Igorplisko <igorplisko95@gmail.com>
2020-12-14 20:51:32 +0200

    Commit_12_14_20

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -2,11 +2,13 @@ import { combineReducers, createStore } from "redux";
 import dialogsReducer from "./dialogs-reducer";
 import profileReducer from "./profile-reducer";
 import sidebarReducer from "./sidebar-reducer";
+import usersReducer from "./users_reducer";
 
 let reducers = combineReducers({
    profilePage: profileReducer,
    dialogsPage: dialogsReducer,
-   sidebarReducer: sidebarReducer
+   sidebarReducer: sidebarReducer,
+   usersPage: usersReducer
 
 });
 
