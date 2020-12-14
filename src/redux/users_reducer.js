24039a44c3316d634e22587286337e67c0b5252d
Igorplisko <igorplisko95@gmail.com>
2020-12-14 20:51:43 +0200

    Commit_12_14_20

--- src/redux/users_reducer.js
+++ src/redux/users_reducer.js
@@ -3,35 +3,9 @@ const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET_USERS';
 
 let initialState = {
+   users: []
+};
 
-   users: [
-      {
-         id: 1,
-         // photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Dmitry_Nagiev_2017_4.jpg/220px-Dmitry_Nagiev_2017_4.jpg',
-         followed: false,
-         fullName: 'Igor',
-         status: 'When if not now?',
-         location: { city: 'Minsk', country: 'Belarus' }
-      },
-      {
-         id: 2,
-         // photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Dmitry_Nagiev_2017_4.jpg/220px-Dmitry_Nagiev_2017_4.jpg',
-         followed: true,
-         fullName: 'Vlad',
-         status: 'I am a boss too',
-         location: { city: 'Moscow', country: 'Russia' }
-      },
-      {
-         id: 3,
-         // photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Dmitry_Nagiev_2017_4.jpg/220px-Dmitry_Nagiev_2017_4.jpg',
-         followed: false,
-         fullName: 'Alexsandr',
-         status: 'I am a boss too',
-         location: { city: 'Kiev', country: 'Ukraine' }
-      }
-
-   ]
-}
 const usersReducer = (state = initialState, action) => {
    switch (action.type) {
       case FOLLOW:
@@ -62,16 +36,12 @@ const usersReducer = (state = initialState, action) => {
    }
 }
 
+
 export const followAC = (userId) => ({ type: FOLLOW, userId })
 export const unfollowAC = (userId) => ({ type: UNFOLLOW, userId })
-export const setUsersAC = (users) => ({
-   type: SET_USERS, users
-})
-
+export const setUsersAC = (users) => ({ type: SET_USERS, users })
 
 export default usersReducer;
-
-
 
 
 
