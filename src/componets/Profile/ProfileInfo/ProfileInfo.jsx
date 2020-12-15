8c74667f53a3c7ee5523814e24ca9b055534b833
Igorplisko <igorplisko95@gmail.com>
2020-12-15 20:04:35 +0200

    Commit

--- src/componets/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/componets/Profile/ProfileInfo/ProfileInfo.jsx
@@ -6,7 +6,8 @@ const ProfileInfo = () => {
    return (
       <div>
          <div>
-            <img className={s.imgStyle} src="https://sites.google.com/site/prirodanasevseegooglgfgf/_/rsrc/1463456237313/home/priroda_gory_nebo_ozero_oblaka_81150_1920x1080.jpg" />
+            {/* <img className={s.imgStyle} src="https://sites.google.com/site/prirodanasevseegooglgfgf/_/rsrc/1463456237313/home/priroda_gory_nebo_ozero_oblaka_81150_1920x1080.jpg" /> */}
+            <img className={s.imgStyle} src='https://s1.1zoom.ru/big0/917/403317-svetik.jpg' />
          </div>
 
          <div className={s.descriptionBlock}>
