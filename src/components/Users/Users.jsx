f7b919327149a42660ea15e933c9e427e7bf3cbe
Igorplisko <igorplisko95@gmail.com>
2020-12-15 21:43:45 +0200

    commit

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,6 +1,7 @@
 import * as axios from 'axios';
 import React from 'react';
 import styles from './users.module.css';
+import userPhoto from '../../assets/images/user.png'
 
 let Users = (props) => {
    if (props.users.length === 0) {
@@ -17,7 +18,7 @@ let Users = (props) => {
          props.users.map(u => <div key={u.id}>
             <span>
                <div>
-                  <img src={u.photos.small = ! null ? u.photos.small : ""} className={styles.userPhoto} />
+                  <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto} />
                </div>
                <div>
                   {u.followed
