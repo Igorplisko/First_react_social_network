dbbcbc24b3436382b2586c21fcfdd958cd97ce1b
igorplisko <igorplisko95@gmail.com>
2021-03-31 16:18:10 +0300

    change logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import s from "./Header.module.css";
 const Header = () => {
    return (
       <header className={s.header}>
-         <img src="https://lh3.googleusercontent.com/proxy/dTMrEgNsoHatDjxEOoIf5sWEA-r0re2ljkUWReyw82s1a-HsYt3fnjaUmmv7gbKvqjmJwm5cw7YM149E2blwYd3wOZVSqDZqCciSSFoNI7LRZJvuDkP2oPpkKDY37v4Hhgy0gdr6K3ILUDFRRUdy7tJCfLhJhgvLWIQ_lONSt35ZN1LWiTrq9w" />
+         <img src="https://iconape.com/wp-content/png_logo_vector/mdn.png" />
       </header>)
 }
 
