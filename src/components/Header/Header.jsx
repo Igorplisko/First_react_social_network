caee0072746778a69ad6863c21e2f948712e052e
igorplisko <igorplisko95@gmail.com>
2021-03-31 14:44:24 +0300

    I changed the logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import s from "./Header.module.css";
 const Header = () => {
    return (
       <header className={s.header}>
-         <img src="https://cdnimg.rg.ru/img/content/174/48/56/DB2019AL01916_large_d_850.jpg" />
+         <img src="https://lh3.googleusercontent.com/proxy/dTMrEgNsoHatDjxEOoIf5sWEA-r0re2ljkUWReyw82s1a-HsYt3fnjaUmmv7gbKvqjmJwm5cw7YM149E2blwYd3wOZVSqDZqCciSSFoNI7LRZJvuDkP2oPpkKDY37v4Hhgy0gdr6K3ILUDFRRUdy7tJCfLhJhgvLWIQ_lONSt35ZN1LWiTrq9w" />
       </header>)
 }
 
